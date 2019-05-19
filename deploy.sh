54b04558392936b9487e89c919f96918c8e29d99
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-05-19 19:26:32 -0300

    Evolve deploy file
    Add docker build and push steps
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,7 @@
 # Author: João Pedro Sconetto <sconetto.joao@gmail.com>
 
 docker build -t rethink-data-manager:latest .
+
+docker tag rethink-data-manager:latest radop/rethink-data-manager:latest
+
+docker push radop/rethink-data-manager:latest
